a9c94fd9390502ca2b3c8114afdb1ba67c478523
danielvfo <danielvfo@gmail.com>
2020-05-17 23:46:57 -0300

    added date validations

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,36 @@
 import { Router } from 'express';
 import bodyParser from 'body-parser';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
 const appointmentsRouter = Router();
 appointmentsRouter.use(bodyParser.json());
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
+  const roundDate = startOfHour(parseISO(date));
+
+  const isDateAlreadyBooked = appointments.find(appointment =>
+    isEqual(roundDate, appointment.date)
+  );
+
+  if (isDateAlreadyBooked) {
+    return response.status(400).json({
+      message: 'This date and time already has a booking.',
+    });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: roundDate,
   };
   appointments.push(appointment);
   return response.json(appointment);
